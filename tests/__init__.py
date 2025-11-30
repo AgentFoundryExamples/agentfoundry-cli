1d213eb5ecf0b05b8ab1cdbe0f178cbf53d89061
John Bros <brosnihanjohn@gmail.com>
2025-11-30 01:22:03 -0600

    Header Update

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,3 @@
-# ============================================================
 # SPDX-License-Identifier: GPL-3.0-or-later
 # This program was generated as part of the AgentFoundry project.
 # Copyright (C) 2025  John Brosnihan
@@ -15,5 +14,4 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# ============================================================
 """Tests for Agent Foundry CLI."""
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,3 @@
-# ============================================================
 # SPDX-License-Identifier: GPL-3.0-or-later
 # This program was generated as part of the AgentFoundry project.
 # Copyright (C) 2025  John Brosnihan
@@ -15,7 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# ============================================================
 """
 Tests for the Agent Foundry .af file parser.
 """
